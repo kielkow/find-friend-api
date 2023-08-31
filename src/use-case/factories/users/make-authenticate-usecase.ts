f06f9298b39acb085af437c375b746803770988c
kielkow <matheuskiel@fiorifer.com.br>
2023-08-31 09:36:20 -0300

    refact

--- src/use-case/factories/users/make-authenticate-usecase.ts
+++ src/use-case/factories/users/make-authenticate-usecase.ts
@@ -1,5 +1,5 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '../../usecases/authenticate/authenticate'
+import { AuthenticateUseCase } from '../../usecases/users/authenticate/authenticate'
 
 export function makeAuthenticateUseCase() {
 	const prismaUsersRepository = new PrismaUsersRepository()
