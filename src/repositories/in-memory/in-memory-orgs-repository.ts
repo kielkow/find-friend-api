135aa7ab4f64ea027cf4b767a217a7865dcc4cfb
kielkow <matheuskiel@fiorifer.com.br>
2023-09-06 09:16:45 -0300

    feat: orgs repository methods

---
+++ src/repositories/in-memory/in-memory-orgs-repository.ts
@@ -0,0 +1,44 @@
+import { randomUUID } from 'crypto'
+import { Prisma, Role, ORG } from '@prisma/client'
+
+import { OrgsRepository } from '../orgs-repository'
+
+export class InMemoryOrgsRepository implements OrgsRepository {
+	public orgs: ORG[] = []
+
+	async findById(id: string) {
+		const org = this.orgs.find((org) => org.id === id)
+
+		if (!org) return null
+
+		return org
+	}
+
+	async findByEmail(email: string) {
+		const org = this.orgs.find((org) => org.email === email)
+
+		if (!org) return null
+
+		return org
+	}
+
+	async create(data: Prisma.ORGCreateInput) {
+		const { name, email, password_hash, address, locale, phone } = data
+
+		const org = {
+			id: randomUUID(),
+			name,
+			email,
+			password_hash,
+			address,
+			locale,
+			phone,
+			role: Role.ADMIN,
+			created_at: new Date(),
+		}
+
+		this.orgs.push(org)
+
+		return org
+	}
+}
---
+++ src/repositories/prisma/prisma-orgs-repository.ts
@@ -0,0 +1,21 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
+
+import { OrgsRepository } from '../orgs-repository'
+
+export class PrismaOrgsRepository implements OrgsRepository {
+	async findById(id: string) {
+		const org = await prisma.oRG.findUnique({ where: { id } })
+		return org
+	}
+
+	async findByEmail(email: string) {
+		const org = await prisma.oRG.findUnique({ where: { email } })
+		return org
+	}
+
+	async create(data: Prisma.ORGCreateInput) {
+		const org = await prisma.oRG.create({ data })
+		return org
+	}
+}
