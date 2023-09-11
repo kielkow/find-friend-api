167ab0ac58d0520fac21505cbe75dd10ae59dc9d
kielkow <matheuskiel@fiorifer.com.br>
2023-09-11 09:36:02 -0300

    feat: orgs controller - refresh token

--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -2,9 +2,12 @@ import { FastifyInstance } from 'fastify'
 
 import { register } from './register/register'
 import { authenticate } from './authenticate/authenticate'
+import { refresh } from './refresh/refresh'
 
 export async function orgsRoutes(app: FastifyInstance) {
 	// Not Authenticated
 	app.post('/orgs', register)
 	app.post('/orgs/sessions', authenticate)
+
+	app.patch('/orgs/token/refresh', refresh)
 }
