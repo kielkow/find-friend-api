51f4482daf131199e65255c9714ab8e4e87e8a91
kielkow <matheuskiel@fiorifer.com.br>
2023-09-12 10:35:07 -0300

    feat: check all env vars

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,10 +2,23 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
+	JWT_SECRET: z.string().default('findfriendapi'),
+
 	PORT: z.coerce.number().default(3333),
 	NODE_ENV: z.enum(['dev', 'test', 'prod']).default('dev'),
 	HOST: z.string().default('0.0.0.0'),
-	JWT_SECRET: z.string().default('apisolid'),
+
+	POSTGRESQL_DATABASE: z.string().default('findfriendapi'),
+	POSTGRESQL_USERNAME: z.string().default('postgresql'),
+	POSTGRESQL_PASSWORD: z.string().default('postgresql'),
+
+	DATABASE_URL: z
+		.string()
+		.default(
+			'postgresql://postgresql:postgresql@postgresql:5432/findfriendapi?schema=public',
+		),
+
+	REDIS_PASSWORD: z.string().default('redis'),
 })
 
 const _env = envSchema.safeParse(process.env)
