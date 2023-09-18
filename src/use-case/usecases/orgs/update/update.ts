0a05cadf05ad72223a5c9b10f8ddcc2c2392fb61
kielkow <matheuskiel@fiorifer.com.br>
2023-09-18 18:32:59 -0300

    feat: org usecase - update

---
+++ src/use-case/usecases/orgs/update/update.ts
@@ -0,0 +1,57 @@
+import { hash } from 'bcryptjs'
+import { Locale, ORG, Role } from '@prisma/client'
+
+import { OrgsRepository } from '@/repositories/orgs-repository'
+
+import { ResourceNotFoundError } from '@/use-case/errors/resource-not-found-error'
+
+interface UpdateUseCaseRequest {
+	id: string
+	name?: string
+	email?: string
+	password?: string
+	role?: Role
+	address?: string
+	phone?: string
+	locale?: Locale
+}
+
+interface UpdateUseCaseResponse {
+	org: ORG
+}
+
+export class UpdateUseCase {
+	constructor(private orgsRepository: OrgsRepository) {}
+
+	async execute({
+		id,
+		name,
+		email,
+		password,
+		role,
+		address,
+		phone,
+		locale,
+	}: UpdateUseCaseRequest): Promise<UpdateUseCaseResponse> {
+		const orgExists = await this.orgsRepository.findById(id)
+		if (!orgExists) throw new ResourceNotFoundError()
+
+		const password_hash = password
+			? await hash(password, 6)
+			: orgExists.password_hash
+
+		const org = await this.orgsRepository.update({
+			id,
+			name: name || orgExists.name,
+			email: email || orgExists.email,
+			password_hash,
+			role: role || orgExists.role,
+			address: address || orgExists.address,
+			phone: phone || orgExists.phone,
+			locale: locale || orgExists.locale,
+		})
+		if (!org) throw new ResourceNotFoundError()
+
+		return { org }
+	}
+}
